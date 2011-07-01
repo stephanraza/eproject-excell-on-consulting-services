4e3398fdf8d251cd255ce212a007ec2212954507
anhlt_c00177 <anhlt_c00177@fpt.aptech.ac.vn>
2011-07-01 10:15:53 +0000

    Entity

---
+++ News.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eproject.ECS.Entities
+{
+    public class News
+    {
+
+        #region Private member
+        private string news_id;
+        private string news_title;
+        private string news_content;
+        private DateTime news_date;
+        private bool news_is_delete;
+        #endregion
+
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NewsId
+        {
+            get { return news_id; }
+            set { news_id = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NewsTitle
+        {
+            get { return news_title; }
+            set { news_title = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NewsContent
+        {
+            get { return news_content; }
+            set { news_content = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime NewsDate
+        {
+            get { return news_date; }
+            set { news_date = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool NewsIsDelete
+        {
+            get { return news_is_delete; }
+            set { news_is_delete = value; }
+        }
+
+        #endregion
+
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public News()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newsId"></param>
+        /// <param name="newsTitle"></param>
+        /// <param name="newsContent"></param>
+        /// <param name="newsDate"></param>
+        /// <param name="newsIsDelete"></param>
+        public News(string newsId, string newsTitle, string newsContent, DateTime newsDate, bool newsIsDelete)
+        {
+            news_id = newsId;
+            news_title = newsTitle;
+            news_content = newsContent;
+            news_date = newsDate;
+            news_is_delete = newsIsDelete;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eproject.ECS.Entities
+{
+    public class Profile
+    {
+
+        #region Private member
+        private string profile_id;
+        private string employee_id;
+        private string profile_firt_name;
+        private string profile_last_name;
+        private bool profile_gender;
+        private DateTime profile_date_of_birth;
+        private string profile_address;
+        private string profile_phone_number;
+        private string profile_email;
+        private string profile_avatar;
+        private bool profile_is_delete;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileId
+        {
+            get { return profile_id; }
+            set { profile_id = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string EmployeeId
+        {
+            get { return employee_id; }
+            set { employee_id = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileFirtName
+        {
+            get { return profile_firt_name; }
+            set { profile_firt_name = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileLastName
+        {
+            get { return profile_last_name; }
+            set { profile_last_name = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool ProfileGender
+        {
+            get { return profile_gender; }
+            set { profile_gender = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime ProfileDateOfBirth
+        {
+            get { return profile_date_of_birth; }
+            set { profile_date_of_birth = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileAddress
+        {
+            get { return profile_address; }
+            set { profile_address = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfilePhoneNumber
+        {
+            get { return profile_phone_number; }
+            set { profile_phone_number = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileEmail
+        {
+            get { return profile_email; }
+            set { profile_email = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProfileAvatar
+        {
+            get { return profile_avatar; }
+            set { profile_avatar = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool ProfileIsDelete
+        {
+            get { return profile_is_delete; }
+            set { profile_is_delete = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public Profile()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="profileId"></param>
+        /// <param name="employeeId"></param>
+        /// <param name="profileFirtName"></param>
+        /// <param name="profileLastName"></param>
+        /// <param name="profileGender"></param>
+        /// <param name="profileDateOfBirth"></param>
+        /// <param name="profileAddress"></param>
+        /// <param name="profilePhoneNumber"></param>
+        /// <param name="profileEmail"></param>
+        /// <param name="profileAvatar"></param>
+        /// <param name="profileIsDelete"></param>
+        public Profile(string profileId, string employeeId, string profileFirtName, string profileLastName, bool profileGender, DateTime profileDateOfBirth, string profileAddress, string profilePhoneNumber, string profileEmail, string profileAvatar, bool profileIsDelete)
+        {
+            profile_id = profileId;
+            employee_id = employeeId;
+            profile_firt_name = profileFirtName;
+            profile_last_name = profileLastName;
+            profile_gender = profileGender;
+            profile_date_of_birth = profileDateOfBirth;
+            profile_address = profileAddress;
+            profile_phone_number = profilePhoneNumber;
+            profile_email = profileEmail;
+            profile_avatar = profileAvatar;
+            profile_is_delete = profileIsDelete;
+        }
+
+        #endregion
+    }
+}
