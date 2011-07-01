8fb8503e0d8bc235edebf24f2d9004eea7360c9e
hoangdp_c00473 <hoangdp_c00473@fpt.aptech.ac.vn>
2011-07-01 14:11:18 +0000

    BLL, DAL, ENTITY

---
+++ Source code/EProject.ECS/Eproject.ECS.Dal/Provider/CustomizeRoleProvider.cs
@@ -0,0 +1,79 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AppConfiguration.cs">
+//   Copyright 2011 - hoangdp_c00473. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace Eproject.ECS.Dal.Provider
+{
+    public class CustomizeRoleProvider : RoleProvider
+    {
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source code/EProject.ECS/Eproject.ECS.Entities/Enums/TableType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eproject.ECS.Entities.Enums
+{
+    /// <summary>
+    /// Determines the type of a contact person.
+    /// </summary>
+    public enum TableType
+    {
+        /// <summary>
+        /// Indicates an unidentified value.
+        /// </summary>
+        Account = 0,
+        /// <summary>
+        /// Indicates a friend.
+        /// </summary>
+        Category = 1,
+        /// <summary>
+        /// Indicates a family member.
+        /// </summary>
+        Family = 2,
+        /// <summary>
+        /// Indicates a co worker.
+        /// </summary>
+        Colleague = 3
+    }
+}
