9900f1f99bc5127fbc321c877634e56ca6d647da
anhlt_c00177@fpt.aptech.ac.vn <anhlt_c00177@fpt.aptech.ac.vn@0f5341df-0329-57fb-3981-dc20c974b56b>
2011-07-01 13:33:29 +0000

    Entity da xong. Mien spam

---
+++ trunk/Source code/EProject.ECS/Eproject.ECS.Entities/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eproject.ECS.Entities
+{
+    public class Employee
+    {
+
+        #region Private Memember
+        private string employee_id;
+        private string department_id;
+        private bool employee_is_delete;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public string EmployeeId
+        {
+            get { return employee_id; }
+            set { employee_id = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DepartmentId
+        {
+            get { return department_id; }
+            set { department_id = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool EmployeeIsDelete
+        {
+            get { return employee_is_delete; }
+            set { employee_is_delete = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public Employee()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <param name="departmentId"></param>
+        /// <param name="employeeIsDelete"></param>
+        public Employee(string employeeId, string departmentId, bool employeeIsDelete)
+        {
+            employee_id = employeeId;
+            department_id = departmentId;
+            employee_is_delete = employeeIsDelete;
+        }
+
+        #endregion
+
+    }
+}
