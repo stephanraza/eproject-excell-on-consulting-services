386994c85a3a5e063a03ad8acd931f130da5f5a6
hoangdp_c00473@fpt.aptech.ac.vn <hoangdp_c00473@fpt.aptech.ac.vn@0f5341df-0329-57fb-3981-dc20c974b56b>
2011-07-02 17:13:04 +0000

    Sửa lại project Entities

--- trunk/Source code/Eproject.ECS/Eproject.ECS.Entities/Department.cs
+++ trunk/Source code/Eproject.ECS/Eproject.ECS.Entities/Department.cs
@@ -7,74 +7,9 @@ namespace Eproject.ECS.Entities
 {
     public class Department
     {
-
-        #region Private member
-        private string Department_Id;
-        private string Department_Name;
-        private string Department_Description;
-        private bool Department_IsDelete;
-        #endregion
-
-        #region properties
-        /// <summary>
-        /// 
-        /// </summary>
-        public string DepartmentId
-        {
-            get { return Department_Id; }
-            set { Department_Id = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string DepartmentName
-        {
-            get { return Department_Name; }
-            set { Department_Name = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string DepartmentDescription
-        {
-            get { return Department_Description; }
-            set { Department_Description = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool DepartmentIsDelete
-        {
-            get { return Department_IsDelete; }
-            set { Department_IsDelete = value; }
-        }
-
-        #endregion
-
-        #region Constructor
-        /// <summary>
-        /// 
-        /// </summary>
-        public Department()
-        {
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="departmentId"></param>
-        /// <param name="departmentName"></param>
-        /// <param name="departmentDescription"></param>
-        /// <param name="departmentIsDelete"></param>
-        public Department(string departmentId, string departmentName, string departmentDescription, bool departmentIsDelete)
-        {
-            Department_Id = departmentId;
-            Department_Name = departmentName;
-            Department_Description = departmentDescription;
-            Department_IsDelete = departmentIsDelete;
-        }
-
-        #endregion
-
+        public String Department_Id { get; set; }
+        public String Department_Name { get; set; }
+        public String Department_Description { get; set; }
+        public bool Department_IsDelete { get; set; }
     }
 }
--- trunk/Source code/Eproject.ECS/Eproject.ECS.Entities/Problem.cs
+++ trunk/Source code/Eproject.ECS/Eproject.ECS.Entities/Problem.cs
@@ -7,85 +7,10 @@ namespace Eproject.ECS.Entities
 {
     public class Problem
     {
-
-        #region Private member
-        private string problem_id;
-        private string customer_id;
-        private string problem_title;
-        private string problem_content;
-        private bool problem_is_delete;
-
-        #endregion
-
-        #region Properties
-        /// <summary>
-        /// 
-        /// </summary>
-        public string ProblemId
-        {
-            get { return problem_id; }
-            set { problem_id = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string CustomerId
-        {
-            get { return customer_id; }
-            set { customer_id = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string ProblemTitle
-        {
-            get { return problem_title; }
-            set { problem_title = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string ProblemContent
-        {
-            get { return problem_content; }
-            set { problem_content = value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool ProblemIsDelete
-        {
-            get { return problem_is_delete; }
-            set { problem_is_delete = value; }
-        }
-
-        #endregion
-
-        #region Private member
-        /// <summary>
-        /// 
-        /// </summary>
-        public Problem()
-        {
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="problemId"></param>
-        /// <param name="customerId"></param>
-        /// <param name="problemTitle"></param>
-        /// <param name="problemContent"></param>
-        /// <param name="problemIsDelete"></param>
-        public Problem(string problemId, string customerId, string problemTitle, string problemContent, bool problemIsDelete)
-        {
-            problem_id = problemId;
-            customer_id = customerId;
-            problem_title = problemTitle;
-            problem_content = problemContent;
-            problem_is_delete = problemIsDelete;
-        }
-
-        #endregion
-
+        public String Problem_Id { get; set; }
+        public String Customer_Id { get; set; }
+        public String Problem_Title { get; set; }
+        public String Problem_Content { get; set; }
+        public bool Problem_IsDelete { get; set; }
     }
 }
