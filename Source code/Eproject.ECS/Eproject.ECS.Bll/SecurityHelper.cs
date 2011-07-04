4ff21b4ec8557f991aa3cd24388eb3890f076818
hoangdp_c00473 <hoangdp_c00473@fpt.aptech.ac.vn>
2011-07-04 16:40:24 +0000

    SecurityHelper.cs dùng để mã hóa

---
+++ Source code/Eproject.ECS/Eproject.ECS.Bll/SecurityHelper.cs
@@ -0,0 +1,191 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SecurityHelper.cs">
+//   Copyright 2011 - hoangdp_c00473. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace Eproject.ECS.Bll
+{
+    /// <summary>
+    /// The SecurityHelper.cs class support encryption and decrypt datas.
+    /// </summary>
+    public class SecurityHelper
+    {
+        /// <summary>
+        /// SingleTon pattern.
+        /// </summary>
+        private static SecurityHelper instance;
+
+        public static SecurityHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SecurityHelper();
+                return instance;
+            }
+        }        
+        /// <summary>
+        /// Constructor of SecurityHelper class.
+        /// </summary>
+        private SecurityHelper()
+        {
+            md5Hash = MD5.Create();
+        }
+
+        private static MD5 md5Hash;
+
+        /// <summary>
+        /// Get string was encripted by MD5 but it could be not decripted.
+        /// </summary>
+        /// <param name="input">Input string to encript.</param>
+        /// <returns>Return string was encripted by MD5.</returns>
+        public string GetMd5Hash(string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Verify a hash against a string.
+        /// </summary>
+        /// <param name="input">Input string to check verify.</param>
+        /// <param name="hash">String encripted in database.</param>
+        /// <returns>Return true if 2 string are match, false otherwise.</returns>
+        public bool VerifyMd5Hash(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Get string was encripted by Cryptography and it could be decripted.
+        /// </summary>
+        /// <param name="toEncrypt">Input string to encript.</param>
+        /// <param name="useHashing">Using hash algorithm.</param>
+        /// <returns></returns>
+        public string EncryptCryptography(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
+            
+            // Get the key from config file
+            string key = (string)settingsReader.GetValue("SecurityKey", typeof(String));
+            
+            //If hashing use get hashcode regards to your key
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //Always release the resources and flush data
+                // of the Cryptographic service provide. Best Practice
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0,
+              toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        /// <summary>
+        /// Get string that was decripted by Cryptography.
+        /// </summary>
+        /// <param name="cipherString">Input string to decript.</param>
+        /// <param name="useHashing">Using hash algorithm.</param>
+        /// <returns></returns>
+        public string DecryptCryptography(string cipherString, bool useHashing)
+        {
+            byte[] keyArray;
+            //get the byte code of the string
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
+            //Get your key from config file to open the lock!
+            string key = (string)settingsReader.GetValue("SecurityKey", typeof(String));
+
+            if (useHashing)
+            {
+                //if hashing was used get the hash code with regards to your key
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //release any resource held by the MD5CryptoServiceProvider
+
+                hashmd5.Clear();
+            }
+            else
+            {
+                //if hashing was not implemented get the byte code of the key
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. 
+            //We choose ECB(Electronic code Book)
+
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor                
+            tdes.Clear();
+            //return the Clear decrypted TEXT
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
